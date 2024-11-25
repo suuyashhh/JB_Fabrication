979df1f1b7b25d2733ec01c2e108ba34abaaf546
Suyash Patil <144991357+suuyashhh@users.noreply.github.com>
2024-11-25 16:20:22 +0530

    Update Fab_Admin_Transport.aspx.cs

--- Fab_Admin_Transport.aspx.cs
+++ Fab_Admin_Transport.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
@@ -25,35 +25,36 @@ namespace WebApplication1
 
         protected void btnSubmitBill_Click(object sender, EventArgs e)
         {
-            //con.Close();
-            //SqlCommand cmd = new SqlCommand("insert into Bill (user_id,animal_type,price,date) values (@id,@animal_type,@rs,@dt)", con);
+            con.Close();
+            SqlCommand cmd = new SqlCommand("insert into Fab_Expanse (Exp_name,Exp_price,date,User_id) values (@expname,@expprice,@dt,@ui)", con);
 
 
-            //cmd.Parameters.AddWithValue("@id", Session["userId"]);
-            //cmd.Parameters.AddWithValue("@animal_type", ddlbill.Text);
-            //cmd.Parameters.AddWithValue("@rs", billPrice.Text);
+            cmd.Parameters.AddWithValue("@ui", 2);
+            cmd.Parameters.AddWithValue("@expname", Tplace.Text);
+            cmd.Parameters.AddWithValue("@rs", billPrice.Text);
 
 
-            //DateTime selectedDate;
-            //if (DateTime.TryParse(billDate.Text, out selectedDate))
-            //{
-            //    cmd.Parameters.AddWithValue("@dt", selectedDate);
-            //}
-            //else
-            //{
+            DateTime selectedDate;
+            if (DateTime.TryParse(billDate.Text, out selectedDate))
+            {
+                cmd.Parameters.AddWithValue("@dt", selectedDate);
+            }
+            else
+            {
 
-            //    Response.Write("<script>alert('Invalid date format!');</script>");
-            //    return;
-            //}
-
-
-            //con.Open();
-            //cmd.ExecuteNonQuery();
+                Response.Write("<script>alert('Invalid date format!');</script>");
+                return;
+            }
 
 
-            //billPrice.Text = "";            
-            //billDate.Text = "";
-            //this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Record saved successfully','','success');", true);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+
+            billPrice.Text = "";
+            Tplace.Text = "";
+            billDate.Text = "";
+            this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Record saved successfully','','success');", true);
         }
     }
 }
